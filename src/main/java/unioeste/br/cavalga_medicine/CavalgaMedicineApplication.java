9283fbb699b57978b78c4c4e85b40ad6fbc84aae
Nikoly Cover <nikoly.pereira@pti.org.br>
2024-10-30 22:25:09 -0300

    first commit

---
+++ src/main/java/unioeste/br/cavalga_medicine/CavalgaMedicineApplication.java
@@ -0,0 +1,13 @@
+package unioeste.br.cavalga_medicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CavalgaMedicineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CavalgaMedicineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unioeste/br/cavalga_medicine/CavalgaMedicineApplicationTests.java
@@ -0,0 +1,13 @@
+package unioeste.br.cavalga_medicine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CavalgaMedicineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
